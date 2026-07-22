7b8f5d1c787dfbec0bf3546c58f6e773bd5a6509
ponomarev.dv <graiwers@gmail.com>
2026-07-22 17:10:11 +0400

    Spr1_7

---
+++ sprint1/src/Spr1_6.java
@@ -0,0 +1,49 @@
+public class Spr1_6 {
+    static void main() {
+//        Парсинг. Преобразование строки в число
+        String input = "1000";
+        Integer number = Integer.parseInt(input);
+
+        Byte.parseByte("12");
+        Short.parseShort("345");
+        Integer.parseInt("99999");
+        Long.parseLong("100000000000");
+        Float.parseFloat("12.3");
+        Double.parseDouble("456.789");
+        Boolean.parseBoolean("true");
+
+        long a = 124_324_444;
+        long b = 7_535_343_443L;
+        long maximum = Long.max(a,b);
+        System.out.println("Maximum: " + maximum);
+        long minimum = Long.min(a, b);
+        System.out.println("Minimum: " + minimum);
+
+        // привеление классов орберток к примитиву
+        Long bigNumber = 10l;
+        short smallNumber = bigNumber.shortValue();
+
+
+
+        byte c = 10;
+        byte d = 20;
+        System.out.println(findMax(c,d));
+
+
+        String firstNumber = "123.45";
+        String secondNumber = "345.67";
+        System.out.println(addNumber(firstNumber, secondNumber));
+
+    }
+
+    private static byte findMax(byte firstNumber, byte secondNumber){
+        return (byte) Integer.max(firstNumber, secondNumber);
+    }
+
+
+    private static Float addNumber(String firstNumber, String secondNumber){
+        return Float.parseFloat(firstNumber) + Float.parseFloat(secondNumber);
+    }
+
+
+}
---
+++ sprint1/src/Spr1_7.java
@@ -0,0 +1,51 @@
+import javax.smartcardio.Card;
+
+public class Spr1_7 {
+    public static void main(String[] args) {
+// передача в метод по ссылке и значению
+        int number = 10;
+        changeVariable(number); //внутри метода число изменится, но ничего не вернет
+         System.out.println(number); //10
+
+        number = changeVariable2(number); //внутри метода число изменится и результат передастся сюда
+        System.out.println(number); //30
+
+
+        Cat pixel = new Cat("Red"); // создается рыжий кот
+        changeColor(pixel); //передается обьект в метод
+        System.out.println(pixel.color); // кот черный
+
+
+        Integer number2 = 10;
+        changeVariable3(number2);
+        System.out.println(number2);
+
+
+    }
+    private static void changeVariable(int variable){
+        variable = variable * 3; //значение variable стало 30, но мы ничего не возврашаем
+    }
+
+    private static int changeVariable2(int variable){
+        return variable = variable * 3; //значение variable стало 30, но мы ничего не возврашаем
+    }
+
+    private static void changeVariable3(Integer variable){
+         variable = variable * 3;
+         //значение variable стало 30, но при умножение создалось int число
+        // при сохранение числа в переменную Integer создается новый обьект.
+        // в переменную variable сохранена ссылка на новое значение,
+        // а переменная number остается ссылка на старое
+    }
+
+    private static void changeColor(Cat someCat){ //метод принимает обьект класса
+        someCat.color = "Black"; //и меняет ему цвет
+    }
+}
+
+class Cat{
+    String color;
+    public Cat(String catColor){
+        color = catColor;
+    }
+}
