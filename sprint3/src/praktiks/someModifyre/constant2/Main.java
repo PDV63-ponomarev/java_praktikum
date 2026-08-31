e09576bfdbd161889e856bd357508977c814b241
Dmitiy <graiwers@gmail.com>
2026-08-31 13:02:19 +0400

    praktiks constant

---
+++ sprint3/src/praktiks/someModifyre/constant2/Main.java
@@ -0,0 +1,17 @@
+package praktiks.someModifyre.constant2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Пожалуйста, введите сумму перевода в рублях.");
+    // считайте сумму перевода
+
+    boolean isValid = ... // добавьте вызов метод isValidAmount
+    if (isValid)
+        System.out.println("Спасибо! Ваш перевод на сумму " + amount + " р. успешно выполнен.");
+}
+}
+}
---
+++ sprint3/src/praktiks/someModifyre/constant2/TransactionValidator.java
@@ -0,0 +1,10 @@
+package praktiks.someModifyre.constant2;
+
+public class TransactionValidator {
+    // объявите константы
+
+    // объявите метод isValidAmount()
+    // внутри метода добавьте проверки на минимальную и максимальную сумму перевода
+    System.out.println("Минимальная сумма перевода: " + ... + " р. Попробуйте ещё раз!");
+    System.out.println("Максимальная сумма перевода: " + ... + " р. Попробуйте ещё раз!");
+}
