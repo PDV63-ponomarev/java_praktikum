6783d38c7d4ac09b6e4c749a7b0204d293f4de08
Dmitiy <graiwers@gmail.com>
2026-09-03 16:54:55 +0400

    practic referense

---
+++ sprint3/src/praktiks/OOP/referense_type4/Calculator.java
@@ -0,0 +1,14 @@
+package praktiks.OOP.referense_type4;
+
+import java.util.List;
+
+public class Calculator {
+    private Calculator() {}
+
+    public static int calculate(List<MediaItem> mediaItems) {
+        int day = 0;
+        // Напишите реализацию метода, который будет возвращать общее количество дней,
+        // потраченных на просмотр фильмов и сериалов
+        return day;
+    }
+}
---
+++ sprint3/src/praktiks/OOP/referense_type4/MediaItem.java
@@ -0,0 +1,10 @@
+package praktiks.OOP.referense_type4;
+
+public interface MediaItem {
+
+    // Длительность фильма или сериала в минутах
+    public int getRuntime();
+
+    public String getTitle();
+
+}
