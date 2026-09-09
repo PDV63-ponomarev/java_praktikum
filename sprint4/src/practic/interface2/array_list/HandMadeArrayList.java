cd0be4ec6f81647e4640c3f5a9edd81dd2ca0b77
Dmitiy <graiwers@gmail.com>
2026-09-09 16:46:21 +0400

    LinkedList

--- sprint4/src/practic/interface2/array_list/HandMadeArrayList.java
+++ sprint4/src/practic/interface2/array_list/HandMadeArrayList.java
@@ -15,12 +15,14 @@ public class HandMadeArrayList<T> {
      */
     public void add(T newElement) {
         // Допишите проверку
-        if (... == ...) {
+        if (elements.length == size) {
             grow();
         }
 
-				/* Допишите код, который добавит очередной элемент в массив
+	/* Допишите код, который добавит очередной элемент в массив
            и увеличит размер массива на единицу.*/
+        elements[size] = newElement;
+        size++;
     }
 
     public T get(int index) {
@@ -40,6 +42,9 @@ public class HandMadeArrayList<T> {
         // Допишите цикл, который копирует все элементы из массива elements в новый массив newArray
         // Копируем элементы из старого массива в новый
 
+        for (int i = 0; i < elements.length; i++) {
+            newArray[i] = elements[i];
+        }
 
         // Возвращаемый новый массив
         this.elements = newArray;
