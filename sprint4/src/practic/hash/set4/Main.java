727d01defb78c5ff252f0ebc220dda6254fe7c1f
Dmitiy <graiwers@gmail.com>
2026-09-17 16:53:38 +0400

    hash set

---
+++ sprint4/src/practic/hash/set4/Main.java
@@ -0,0 +1,34 @@
+package practic.hash.set4;
+
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    private static List<String> allPurchases = List.of(
+            "яблоки",
+            "молоко",
+            "колбаса",
+            "огурцы",
+            "сок",
+            "хлеб",
+            "виноград",
+            "молоко",
+            "йогурт",
+            "хлеб",
+            "пельмени"
+    );
+
+    public static void main(String[] args) {
+        // переменная uniquePurchases должна содержать множество уникальных товаров
+        ... uniquePurchases = ...
+
+        // допишите вывод количества уникальных товаров
+        System.out.println( "За месяц было куплено " + ... + " уникальных товаров.");
+    }
+
+    // реализуйте этот метод
+    public static ... findUniquePurchases(List<String> allPurchases) {
+        ...
+    }
+}
+}
